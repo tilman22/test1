f2885032a8346ea2ed72c6a8c75fda8d6034e69e
tilman22 <jdoe@email.com>
2017-04-07 14:39:41 +0200

    test

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  test1
+ *
+ *        Version:  1.0
+ *        Created:  04/07/2017 02:33:00 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
